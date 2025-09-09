d7c90ee59c22807108180a9cc39cb621f218e6e1
jbhampton1 <126513556+JoBHampton@users.noreply.github.com>
2025-09-09 10:29:00 -0500

    Basic index.html added

---
+++ Fall2025-Project1-jbhampton1/Fall2025-Project1-jbhampton1/Models/ErrorViewModel.cs
@@ -0,0 +1,7 @@
+namespace Fall2025_Project1_jbhampton1.Models {
+    public class ErrorViewModel {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
